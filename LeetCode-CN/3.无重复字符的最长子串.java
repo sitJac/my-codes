fc5f73271426b6311caacf0183c9a6784256ce80
sitjac <46061424+sitJac@users.noreply.github.com>
2021-05-20 09:22:24 +0800

    0003-Longest-Substring-Without-Repeating-Characters

--- LeetCode-CN/3.无重复字符的最长子串.java
+++ LeetCode-CN/3.无重复字符的最长子串.java
@@ -6,23 +6,25 @@
 
 // @lc code=start
 class Solution {
-public:
-    int lengthOfLongestSubstring(string s) {
-        int freq[256] = {0};
-        int l = 0, r = -1; 
-        int res = 0;
-        while(l < s.size()){
-            if(r + 1 < s.size() && freq[s[r+1]] == 0){
-                r++;
-                freq[s[r]]++;
-            }else {
-                freq[s[l]]--;
-                l++;
-            }
-            res = max(res, r-l+1);
+    public int lengthOfLongestSubstring(String s) {
+        // 记录字符上一次出现的位置
+        int[] last = new int[128];
+        for(int i = 0; i < 128; i++) {
+            last[i] = -1;
         }
+        int n = s.length();
+
+        int res = 0;
+        int start = 0; // 窗口开始位置
+        for(int i = 0; i < n; i++) {
+            int index = s.charAt(i);
+            start = Math.max(start, last[index] + 1);
+            res   = Math.max(res, i - start + 1);
+            last[index] = i;
+        }
+
         return res;
     }
-};
+}
 // @lc code=end
 
