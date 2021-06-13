b9dcfa2cf32b443d22afd8640126fdeba2af6084
sitJac <46061424+sitJac@users.noreply.github.com>
2021-06-13 09:23:12 +0800

    0026-remove-duplicates-from-sorted-array

---
+++ LeetCode-CN/26.删除有序数组中的重复项.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=26 lang=java
+ *
+ * [26] 删除有序数组中的重复项
+ */
+
+// @lc code=start
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int fast = 1, slow = 1;
+        while (fast < n) {
+            if (nums[fast] != nums[fast - 1]) {
+                nums[slow] = nums[fast];
+                ++slow;
+            }
+            ++fast;
+        }
+        return slow;
+    }
+}
+// @lc code=end
+
