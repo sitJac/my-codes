272c0afdbbeb3fcd31a16cf4d91270ae83cd2be9
sitJac <46061424+sitJac@users.noreply.github.com>
2021-07-05 08:44:06 +0800

    1.A+B Problem

--- ECNU-OJ/1.A+B Problem.cpp
+++ ECNU-OJ/1.A+B Problem.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
+# include <iostream>
+
 using namespace std;
 
-int main()
-{
-    int a,b;
+int main(){
+    int a, b;
     cin >> a >> b;
     cout << a+b << endl;
     return 0;
