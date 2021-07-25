d18642ea5b1a33f333d9f158761ec28a242d50e2
sitJac <46061424+sitJac@users.noreply.github.com>
2021-07-25 18:14:34 +0800

    0064-minimum-path-sum

---
+++ LeetCode-CN/64.最小路径和.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=64 lang=java
+ *
+ * [64] 最小路径和
+ */
+
+// @lc code=start
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int width = grid[0].length, high = grid.length;
+        if (high == 0 || width == 0) return 0;
+        // 初始化
+        for (int i = 1; i < high; i++) grid[i][0] += grid[i - 1][0];
+        for (int i = 1; i < width; i++) grid[0][i] += grid[0][i - 1];
+        for (int i = 1; i < high; i++)
+            for (int j = 1; j < width; j++)
+                grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+        return grid[high - 1][width - 1];
+    }
+}
+// @lc code=end
+
