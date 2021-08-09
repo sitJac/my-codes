fd1f8dab134d16557a141e9c3ee0147b092e3f0a
sitJac <xiejunjie914@gmail.com>
2021-08-09 10:24:31 +0800

    0080-remove-duplicates-from-sorted-array-ii

---
+++ LeetCode-CN/80.删除有序数组中的重复项-ii.java
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=80 lang=java
+ *
+ * [80] 删除有序数组中的重复项 II
+ */
+
+// @lc code=start
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+        for (int n : nums)
+            if (i < 2 || n > nums[i-2])
+                nums[i++] = n;
+        return i;
+    }
+}
+// @lc code=end
+
