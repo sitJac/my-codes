4a8782ff41680f9a4feaa5a8003ee0f679033606
sitjac <xiejunjie914@gmail.com>
2021-09-11 15:54:39 +0800

    0092-reverse-linkedlist-ii

---
+++ LeetCode-CPP/92.反转链表-ii.cpp
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=92 lang=cpp
+ *
+ * [92] 反转链表 II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *reverseBetween(ListNode *head, int left, int right) {
+        // 设置 dummyNode 是这一类问题的一般做法
+        ListNode *dummyNode = new ListNode(-1);
+        dummyNode->next = head;
+        ListNode *pre = dummyNode;
+        for (int i = 0; i < left - 1; i++) {
+            pre = pre->next;
+        }
+        ListNode *cur = pre->next;
+        ListNode *next;
+        for (int i = 0; i < right - left; i++) {
+            next = cur->next;
+            cur->next = next->next;
+            next->next = pre->next;
+            pre->next = next;
+        }
+        return dummyNode->next;
+    }
+};
+// @lc code=end
+
