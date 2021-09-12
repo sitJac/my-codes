fb14254fe05b1d2f56f44774d6fac80d1dc5abb0
sitjac <xiejunjie914@gmail.com>
2021-09-12 20:47:39 +0800

    merge-sorted-array

--- LeetCode-CPP/88.合并两个有序数组.cpp
+++ LeetCode-CPP/88.合并两个有序数组.cpp
@@ -8,10 +8,17 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for(int i=0; i != n; i++){
-            nums1[m+i] = nums2[i];
+        int i = m - 1, j = n - 1;
+        for(int k = m + n - 1; k >= 0; k--){
+            if(j < 0 || (i >= 0 && nums1[i] >= nums2[j])){
+                nums1[k] = nums1[i];
+                i--;
+            }
+            else{
+                nums1[k] = nums2[j];
+                j--;
+            }
         }
-        sort(nums1.begin(), nums1.end());
     }
 };
 // @lc code=end
