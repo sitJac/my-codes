34ab4c2f10989169fd4f50fac66677c423ba3eb7
sitJac <xiejunjie914@gmail.com>
2021-09-14 16:48:27 +0800

    LinkedList find middle node

---
+++ LeetCode-Update/Linked List找到中间节点.java
@@ -0,0 +1,15 @@
+/*
+*   Initialize 2 pointers i and j pointing to the head
+*   While the next move of the faster pointer j is valid(inside bound),move j two steps and i one step forward
+*   return the node at j
+*/
+public ListNode linkedListMiddleNode(ListNode head){
+    ListNode i = head, j = head;
+    While(j != null && j.next != null){
+        i = i.next;
+        j = j.next.next;
+    }
+    return i;
+}
+
+
