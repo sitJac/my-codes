c6e0712638d2e3fa73126c5b9f68ecde259aae4e
sitjac <xiejunjie914@gmail.com>
2021-09-26 23:37:41 +0800

    Linked List找到中间节点

--- LeetCode-TurlingPlanet/Linked List找到中间节点.java
+++ LeetCode-TurlingPlanet/Linked List找到中间节点.java
@@ -3,6 +3,7 @@
 *   While the next move of the faster pointer j is valid(inside bound),move j two steps and i one step forward
 *   return the node at j
 */
+
 public ListNode linkedListMiddleNode(ListNode head){
     ListNode i = head, j = head;
     While(j != null && j.next != null){
