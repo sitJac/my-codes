6d299d4e851797cd384069ff61500bb39d2df47a
sitjac <xiejunjie914@gmail.com>
2021-10-28 13:55:29 +0800

    prefix table

--- KMP/kmp.c
+++ KMP/kmp.c
@@ -1,10 +1,39 @@
 #include<stdio.h>
 
 void prefix_table(char pattern[], int prefix[], int n){
-    
+    prefix[0] = 0;
+    int len = 0;
+    int i = 1;
+    while (i < n)
+    {
+        if(pattern[i] == pattern[len]){
+            len++;
+            prefix[i] = len;
+            i++;
+        }
+        else{
+            if(len > 0){
+                len = prefix[len - 1];
+            }
+            else{
+                prefix[i] = len;
+                i++;
+            }
+        }
+    }
 }
 
 int main(){
+    char pattern[] = "ABABCABAA";
+    int prefix[9];
+    int n = 9;
+    prefix_table(pattern, prefix, n);
+
+    int i;
+    for ( i = 0; i < n; i++){
+        printf("%d\n",prefix[i]);
+    }
+    
 
     return 0;
 }
