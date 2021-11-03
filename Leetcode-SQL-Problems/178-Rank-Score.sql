27eef15ab3a96da3e2ab2a2623927bc238de5dad
sitjac <xiejunjie914@gmail.com>
2021-11-03 12:33:23 +0800

    rank scores

---
+++ Leetcode-SQL-Problems/178-Rank-Score.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT tb1.score AS score, (SELECT COUNT(distinct tb2.score)
+                        FROM Score AS tb2
+                        WHERE tb1.score < tb2.score
+                        ) + 1 AS rank
+FROM Scores AS tb1
+ORDER BY score DESC
