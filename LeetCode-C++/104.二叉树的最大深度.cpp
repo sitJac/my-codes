2e3a9539821acdf514297ed8eb98440809593cab
sitjac <xiejunjie914@gmail.com>
2021-11-15 23:19:32 +0800

    maximum-depth-of-binary-tree

---
+++ LeetCode-C++/104.二叉树的最大深度.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=104 lang=cpp
+ *
+ * [104] 二叉树的最大深度
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(!root){
+            return 0;
+        }
+        int l = 1 + maxDepth(root->left);
+        int r = 1 + maxDepth(root->right);
+        return l > r?l:r;
+
+    }
+};
+// @lc code=end
+
