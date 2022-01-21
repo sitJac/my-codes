fb8cf504d232dcb42c9c0f45a98391eea570520e
sitJac <xiejunjie914@gmail.com>
2022-01-21 20:07:54 +0800

    wangdao exercises

--- Data-Structure/chapter2/2_1_2.cpp
+++ Data-Structure/chapter2/2_1_2.cpp
@@ -1,28 +1,26 @@
 //逆置线性表
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 #define Max 50
-struct sqList
-{
-    int a[Max] = {1,4,6,3,2,8,7};
-    int length = 7;
+struct SqList{
+    int a[Max]={1,4,6,3,2,8,7};
+    int length=7;
 };
-
-void reverse(sqList &L)
+void reverse(SqList &L)
 {
-    int v - L.a[i];
-    L.a[i] = L.a[L.length-i-1];
-    L.a[L.length-i-1] = v;
+    for(int i=0;i<L.length/2;i++)
+    {
+        int v=L.a[i];
+        L.a[i]=L.a[L.length-i-1];
+        L.a[L.length-i-1]=v;
+    }
 }
-
 int main()
 {
-    sqList L;
-    for(int i = 0; i<L.length; i++)
-        cout << L.a[i] << "";
-    cout << endl;
+    SqList L;
+    for(int i=0;i<L.length;i++) cout<<L.a[i]<<" ";
+    cout<<endl;
     reverse(L);
-    for(int i=0; i<L.length; i++)
-        cout << L.a[i] <<" ";
+    for(int i=0;i<L.length;i++) cout<<L.a[i]<<" ";
     return 0;
 }
