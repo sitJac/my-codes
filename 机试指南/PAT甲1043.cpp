f68660e557720d1bb16cd95deede47f74de9cbf3
sitjac <xiejunjie914@gmail.com>
2022-01-26 23:31:09 +0800

    is it a binary search tree

---
+++ 机试指南/PAT甲1043.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#define maxSize 1010
+using namespace std;
+
+typedef struct BTNode
+{
+    struct BTNode* lChild;
+    struct BTNode* rChild;
+    int data;
+};
+
+int N;
+int preorder[maxSize];
+
+void insert1(BTNode * &x, int a)
+{
+    
+}
+
+
