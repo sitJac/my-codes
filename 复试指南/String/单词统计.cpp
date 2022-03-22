37d25577ea8ef110802f0b4eb481282c736ff854
sitjac <xiejunjie914@gmail.com>
2022-03-22 21:22:06 +0800

    word count

---
+++ 复试指南/String/单词统计.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    while(getline(cin, s))
+    {
+        map<string, int> mp;
+        string temp;
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(s[i] == ' ' || s[i] == '.' || s[i] == ',')
+            {
+                if(temp != "")
+                {
+                    mp[temp]++;
+                }
+                temp = "";
+            }
+            else
+            {
+                temp += tolower(s[i]);
+            }
+        }
+        for(auto it = mp.begin(); it != mp.end(); it++)
+        {
+            cout << it->first << ":" << it->second << endl;
+        }
+    }
+    return 0;
+}
