04e449c4cf8e64ef3db4c5d6cf874cd065887739
sitjac <xiejunjie914@gmail.com>
2022-03-31 12:24:39 +0800

    bfs

---
+++ 复试指南/BFS/迷宫问题.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+vector< vector<int> > maze;
+vector< vector<int> > best_path;
+vector< vector<int> > temp_path;
+
+void bfs(int i, int j){
+    if(i < 0 || i >= n || j < 0 || j >= m || maze[i][j] == 1){
+        return ;
+    }
+    maze[i][j] = 1;
+    temp_path.push_back({i, j});
+    if(i == n - 1 && j == m - 1){
+        best_path = temp_path;
+    }
+    bfs(i - 1, j);
+    bfs(i + 1, j);
+    bfs(i, j - 1);
+    bfs(i, j + 1);
+    maze[i][j] = 0;
+    temp_path.pop_back();
+}
+
+int main(){
+    while(cin >> n >> m){
+        maze = vector< vector<int> >(n, vector<int>(m, 0));
+        best_path.clear();
+        temp_path.clear();
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                cin >> maze[i][j];
+            }
+        }
+        bfs(0, 0);
+        for(auto it = best_path.begin(); it != best_path.end(); it++){
+            cout << "(" << (*it)[0] << "," << (*it)[1] << ")" << endl;
+        }
+    }
+    return 0;
+}
