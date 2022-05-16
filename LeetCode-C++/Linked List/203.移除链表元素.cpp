d0a96720cd989c9aa15b0af0f2322298267c7ce7
sitjac <xiejunjie914@gmail.com>
2022-05-16 17:04:13 +0800

    linked list

---
+++ LeetCode-C++/Linked List/203.移除链表元素.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=203 lang=cpp
+ *
+ * [203] 移除链表元素
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode *dummy = new ListNode(-1);
+        dummy->next = head;
+        ListNode *cur = dummy;
+        while(cur->next != nullptr) {
+            if(cur->next->val == val) {
+                cur->next = cur->next ->next;
+            }
+            else {
+                cur = cur->next; 
+            }
+        }
+        return dummy->next;
+    }
+};
+// @lc code=end
+
