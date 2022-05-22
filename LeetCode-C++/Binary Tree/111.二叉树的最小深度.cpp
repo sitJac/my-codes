dd075c68eb63a9d9c4ed54f1a2f44fe9d3f5f7d7
sitjac <xiejunjie914@gmail.com>
2022-05-22 20:27:39 +0800

    tree

---
+++ LeetCode-C++/Binary Tree/111.二叉树的最小深度.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=111 lang=cpp
+ *
+ * [111] 二叉树的最小深度
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(root == nullptr) {
+            return 0;
+        }
+        int l = minDepth(root->left);
+        int r = minDepth(root->right);
+        return l == 0 || r == 0 ? l + r + 1 : min(l, r) + 1;
+    }
+};
+// @lc code=end
+
