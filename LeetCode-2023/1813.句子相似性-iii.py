f5cac5176fc5b7f677d416a025b998017af1f542
sitjac <xiejunjie914@gmail.com>
2023-01-16 23:11:57 +0800

    20230116-areSentencesSimilar

---
+++ LeetCode-2023/1813.句子相似性-iii.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode.cn id=1813 lang=python3
+#
+# [1813] 句子相似性 III
+#
+
+# @lc code=start
+class Solution:
+    def areSentencesSimilar(self, sentence1: str, sentence2: str) -> bool:
+        words1 = sentence1.split()
+        words2 = sentence2.split()
+        i, j = 0, 0
+        while i < len(words1) and i < len(words2) and words1[i] == words2[i]:
+            i += 1
+        while j < len(words1) - i and j < len(words2) - i and words1[-j - 1] == words2[-j - 1]:
+            j += 1
+        return i + j == min(len(words1), len(words2))
+# @lc code=end
+
