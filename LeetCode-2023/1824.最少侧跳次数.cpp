91f11d30f99b0756c89f66a5ee8849c951374d3b
sitjac <xiejunjie914@gmail.com>
2023-01-22 23:08:10 +0800

    20230122-minSideJumps

---
+++ LeetCode-2023/1824.最少侧跳次数.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=1824 lang=cpp
+ *
+ * [1824] 最少侧跳次数
+ */
+
+// @lc code=start
+class Solution {
+    static constexpr int inf = 0x3f3f3f3f;
+public:
+    int minSideJumps(vector<int> &obstacles) {
+        int n = obstacles.size() - 1;
+        vector<int> d = {1, 0, 1};
+        for (int i = 1; i <= n; i++) {
+            int minCnt = inf;
+            for (int j = 0; j < 3; j++) {
+                if (j == obstacles[i] - 1) {
+                    d[j] = inf;
+                } else {
+                    minCnt = min(minCnt, d[j]);
+                }
+            }
+            for (int j = 0; j < 3; j++) {
+                if (j == obstacles[i] - 1) {
+                    continue;
+                }
+                d[j] = min(d[j], minCnt + 1);
+            }
+        }
+        return *min_element(d.begin(), d.end());
+    }
+};
+// @lc code=end
+
