69affe42eb2e88a410ff5e17204cad14a82e698c
sitjac <xiejunjie914@gmail.com>
2023-02-07 23:09:05 +0800

    20230207-alertNames

---
+++ LeetCode-2023/1604.警告一小时内使用相同员工卡大于等于三次的人.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode.cn id=1604 lang=python3
+#
+# [1604] 警告一小时内使用相同员工卡大于等于三次的人
+#
+
+# @lc code=start
+class Solution:
+    def alertNames(self, keyName: List[str], keyTime: List[str]) -> List[str]:
+        timeMap = defaultdict(list)
+        for name, time in zip(keyName, keyTime):
+            hour, minute = int(time[:2]), int(time[3:])
+            timeMap[name].append(hour * 60 + minute)
+
+        ans = []
+        for name, times in timeMap.items():
+            times.sort()
+            if any(t2 - t1 <= 60 for t1, t2 in zip(times, times[2:])):
+                ans.append(name)
+        ans.sort()
+        return ans
+# @lc code=end
+
