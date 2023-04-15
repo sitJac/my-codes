845b9e5f2e9f07c1574b3795f146e054bb6cab3c
sitJac <xiejunjie914@163.com>
2023-04-16 00:25:42 +0800

    buiild some cpp games use makefile cmake and xmake

---
+++ cpp-games/snake/src/main.cpp
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <unistd.h>
+#include <bits/stdc++.h>
+#include "input.h"
+#include "snake.h"
+#include "snake_map.h"
+#include <utility>
+#include "macros.h"
+
+using namespace std;
+
+Snake snake;
+SnakeMap snake_map(&snake);
+
+void initialize()
+{
+    input_init();
+    input_enter_off();
+}
+
+bool is_game_end()
+{
+    bool result = false;
+    pair<int, int> snake_head = snake.snake_head;
+    if (snake_head.first < 0 || snake_head.first >= MAP_HEIGHT || snake_head.second < 0 || snake_head.second >= MAP_WIDTH)
+    {
+        result = true;
+    }
+    if (snake.is_dead)
+    {
+        result = true;
+    }
+    return result;
+}
+
+void game_over()
+{
+    cout << "GAME IS OVER" << endl;
+}
+
+void start_game()
+{
+    while (true)
+    {
+        snake.update_movement();
+        if (is_game_end())
+        {
+            game_over();
+            break;
+        }
+        snake_map.redraw();
+
+        usleep(PAUSE_LENGTH);
+
+        snake.validate_direction();
+    }
+}
+
+int main()
+{
+    initialize();
+    start_game();
+    return 0;
+}
---
+++ cpp-games/sudoku/xmake.lua
@@ -0,0 +1,5 @@
+target("sudoku")
+	set_kind("binary")
+	add_files("src/*.cpp")
+	set_languages("c++11")
+
